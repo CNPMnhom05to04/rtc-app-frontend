0807d71afe2d7cbf68a514a4abb758ce750bdac7
CNPMnhom05to04 <157586244+CNPMnhom05to04@users.noreply.github.com>
2025-11-27 15:15:21 +0700

    remove

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ...các config khác của bạn
+
   eslint: {
     ignoreDuringBuilds: true,
   },
--- src/app/(account)/change-profile/page.tsx
+++
@@ -1,7 +0,0 @@
-
-import ChangeProfile from "@/components/features/account/ChangeProfile";
-
-
-export default function ChangeProfilePage() {
-  return <ChangeProfile />;
-}
